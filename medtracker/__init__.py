8cac1bd58b044636def18b5a359e0fb8532503b4
jgfrigo1 <138257969+jgfrigo1@users.noreply.github.com>
2025-04-22 08:24:39 +0200

    Update __init__.py

--- medtracker/__init__.py
+++ medtracker/__init__.py
@@ -1,9 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+import psycopg2
 
 db = SQLAlchemy()
 
+@event.listens_for(Engine, "engine_connect")
+def ping_connection(connection, branch):
+    if branch:
+        return
+    try:
+        connection.scalar("SELECT 1")
+    except psycopg2.Error:
+        connection.invalidate()
+        connection.scalar("SELECT 1")
+        
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///marks.db").replace("postgres://", "postgresql://")
